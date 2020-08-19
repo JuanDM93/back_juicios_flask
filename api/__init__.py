259dfe2ee0d3d247ae75023b28a8a94cfb2b8415
juandominguez <juan_dm93@hotmail.com>
2020-08-19 17:12:37 -0500

    cors

--- api/__init__.py
+++ api/__init__.py
@@ -1,27 +1,5 @@
-# DB
-from flask_mysqldb import MySQL
-db = MySQL()
-#from flask_sqlalchemy import SQLAlchemy
-#db = SQLAlchemy()
-
-# JWT
-from flask_jwt_extended import JWTManager
-jwt  = JWTManager()
-
-# Cors
-from flask_cors import CORS
-CORS()
-
-# Bcrypt
-from flask_bcrypt import Bcrypt
-bcrypt = Bcrypt()
-
-# Mail
-from flask_mail import Mail
-mail = Mail() 
-
-
 from flask import Flask
+
 
 """Initialize Flask app."""
 def create_app():
@@ -29,7 +7,27 @@ def create_app():
     app = Flask(__name__, instance_relative_config=False)
     app.config.from_object('config.Config')
 
-    db.init_app(app)
+    # DB
+    from flask_mysqldb import MySQL
+    db = MySQL(app)
+    #from flask_sqlalchemy import SQLAlchemy
+    #db = SQLAlchemy()
+
+    # JWT
+    from flask_jwt_extended import JWTManager
+    jwt  = JWTManager(app)
+
+    # Bcrypt
+    from flask_bcrypt import Bcrypt
+    bcrypt = Bcrypt(app)
+
+    # Mail
+    from flask_mail import Mail
+    mail = Mail(app) 
+
+    # Cors
+    from flask_cors import CORS
+    CORS(app, support_credentials=True)
 
     with app.app_context():
         # Import routes
@@ -37,5 +35,6 @@ def create_app():
 
         app.register_blueprint(users.bp)
         app.register_blueprint(b_locals.bp)
+
 
         return app
