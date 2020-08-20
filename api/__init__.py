c763c6a65a41819c795b74df468a9aa09726d602
juandominguez <juan_dm93@hotmail.com>
2020-08-20 01:33:54 -0500

    last_nightly

--- api/__init__.py
+++ api/__init__.py
@@ -4,10 +4,11 @@ from flask_cors import CORS
 """Initialize Flask app."""
 def create_app():
     """Construct the core application."""
-    app = Flask(__name__, instance_relative_config=False)
+    app = Flask(
+        __name__, instance_relative_config=False)
     app.config.from_object('config.Config')
 
-    CORS(app, supports_credentials=True)
+    CORS(app)
 
     with app.app_context():
         # Import routes
