cb515bd62c61841e8ef4c6d1fd59270b60d8e4c3
juandominguez <juan_dm93@hotmail.com>
2020-08-20 18:05:48 -0500

    auth init

--- api/utils/auth.py
+++ api/utils/auth.py
@@ -1,7 +1,14 @@
 # JWT & Bcrypt
+from flask import current_app
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
 
 
+jwt = JWTManager()
+bcrypt = Bcrypt()
+
+
 def init_auth():
-    return JWTManager(), Bcrypt()
+    jwt.init_app(current_app)
+    bcrypt.init_app(current_app)
+    return jwt, bcrypt
