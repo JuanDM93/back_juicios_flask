0d54d37b9510c89df61d310a71efca0228733eac
juandominguez <juan_dm93@hotmail.com>
2020-09-11 12:28:43 -0500

    no get

--- api/routes/files.py
+++ api/routes/files.py
@@ -17,39 +17,28 @@ def allowed_file(filename):
         in current_app.config['ALLOWED_EXTENSIONS']
 
 
-@bp.route('/', methods=['GET', 'POST'])
+@bp.route('/', methods=['POST'])
 def upload_file():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
+    # check if the post request has the file part
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
 
-        pdf = request.files['file']
-        # if user does not select file, browser also
-        # submit an empty part without filename
-        if pdf.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
+    pdf = request.files['file']
+    # if user does not select file, browser also
+    # submit an empty part without filename
+    if pdf.filename == '':
+        flash('No selected file')
+        return redirect(request.url)
 
-        if pdf and allowed_file(pdf.filename):
-            filename = secure_filename(pdf.filename)
-            savepath = os.path.join(
-                current_app.config['UPLOAD_FOLDER'], filename)
-            pdf.save(savepath)
+    if pdf and allowed_file(pdf.filename):
+        filename = secure_filename(pdf.filename)
+        savepath = os.path.join(
+            current_app.config['UPLOAD_FOLDER'], filename)
+        pdf.save(savepath)
 
-            return redirect(
-                url_for('uploads.uploaded_file', filename=filename))
-
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form method=post enctype=multipart/form-data>
-      <input type=file name=file>
-      <input type=submit value=Upload>
-    </form>
-    '''
+        return redirect(
+            url_for('uploads.uploaded_file', filename=filename))
 
 
 @bp.route('/<filename>')
